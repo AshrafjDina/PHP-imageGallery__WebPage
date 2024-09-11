6bf7b0c917a87b9886c349247f7deb7804a3a5bf
AshrafjDina <dinaashrafjunior@gmail.com>
2024-09-11 14:37:13 +0200

    initial commit

---
+++ Gallery Project Material/gallery.php
@@ -0,0 +1,21 @@
+<?php
+include './inc/functions.inc.php';
+include './inc/images.inc.php';
+
+?>
+
+<?php include './views/header.php'; ?> 
+   
+
+
+<div class="gallery-container">
+    <?php foreach ($imageTitles AS $src => $title) : ?>
+
+        <a href="image.php?<?php echo http_build_query(['image' => $src]) ?>" class="gallery-item">
+            <h3><?php echo e($title) ?></h3> 
+            <img src=" ./images/<?php echo rawurldecode($src) ?>" alt="<?php echo e($title) ?>">
+        </a>
+    <?php endforeach ?>
+</div>
+
+<?php include './views/footer.php'; ?>
